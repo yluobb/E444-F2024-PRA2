87a90ce25b82e4c6c9faa5b535168c1a18da12f4
Bonnie <bonnieyl.luo@mail.utoronto.ca>
2024-09-25 01:40:45 -0400

    Add timestamp page using bootstrap
    Add a timestamp page using Bootstrap and Jinja2's templates.

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from datetime import datetime
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
-
-@app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('user.html', name="Bonnie Luo")
