b6fa27128e0f9b8f8068a2b6926026cbc020f828
Bonnie <bonnieyl.luo@mail.utoronto.ca>
2024-09-25 17:27:22 -0400

    Activity 1.4 - Create name form page
    Created a name form page that asks for the user's name, validate the input, and gives a message if the name changes.

--- hello.py
+++ hello.py
@@ -13,6 +13,7 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT email?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 @app.errorhandler(404)
@@ -28,10 +29,23 @@ def internal_server_error(e):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
+    valid_uoft_email = True
+
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
+
+        if 'utoronto' not in form.email.data:
+            valid_uoft_email = False
+        
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+        
         session['name'] = form.name.data
+        session['email'] = form.email.data
+        session['valid_email'] = valid_uoft_email
+
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'), valid_email=session.get('valid_email'))
